6106bcfdb29c4f42f1d88cde5b96e934dc7889bd
ericb <ericb@shoutlet.com>
2012-11-09 09:56:35 -0600

    moved move() call

--- Paddle.js
+++ Paddle.js
@@ -1,10 +1,11 @@
 var Paddle = Koi.extend(BasicBody, {
 	draw: function(){
+	    this.move();
 	    if(this.x < this.bx.min) { this.x = this.bx.min; }
 	    if(this.y < this.by.min) { this.y = this.by.min; }
 	    if((this.x + this.h) > this.bx.max) { this.x = (this.bx.max - this.h); }
 	    if((this.y + this.h) > this.by.max) { this.y = (this.by.max - this.h); }
-		this.move();
+		
 		this.ctx.save();
 		this.ctx.fillStyle = '#00e1ff';
 		this.ctx.fillRect(this.x, this.y, this.w, this.h);
