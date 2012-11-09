debdb1597d55e8c80e0410dfcab2f9696cae1319
Johnny Fuchs <johnny.fuchs@shoutlet.com>
2012-11-08 20:10:12 -0600

    changing 'drawable' to body, not doing any static stuff yet

--- BasicDrawable.js
+++ BasicDrawable.js
@@ -1,9 +1,7 @@
-var BasicDrawable = Koi.define({
+var BasicBody = Koi.define({
 	ctx: 	   null,
 	canvas:    false,
 	name:      null,
-	isStatic:  false,
-	isDrawn:   false,
   
 	init: function(options) {
 		options       = options 	  || {};
@@ -16,23 +14,14 @@ var BasicDrawable = Koi.define({
 		this.canvas   = options.canvas  || defaultCanvas;
 		this.ctx      = options.ctx     || defaultContext;
 		this.name     = options.name    || 'Unnamed Drawable';
-		this.isStatic = options.isStatic  || false;
-		this.isDrawn  = options.isDrawn   || false;
 	},
 
 	draw: function(){
-		if(!this.isStatic || !this.isDrawn){
-			var c = this.ctx;
-			c.save();
-			c.font = '12px';
-			c.fillText(this.name, 0, 12);
-			c.restore();
-			this.isDrawn = true;
-		}
-	},
-	
-	clear: function(){
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.isDrawn = false;
+		var c = this.ctx;
+		c.save();
+		c.font = '12px';
+		c.fillText(this.name, 0, 12);
+		c.restore();
+		this.isDrawn = true;
 	}
 });
