3eba53db92e07a559d0ae0d0f1c667676c651440
Johnny Fuchs <johnny.fuchs@shoutlet.com>
2012-11-08 20:26:30 -0600

    Made BasicDrawable BasicBody, added empty Paddle class

--- BasicBody.js
+++ BasicBody.js
@@ -2,6 +2,13 @@ var BasicBody = Koi.define({
 	ctx: 	   null,
 	canvas:    false,
 	name:      null,
+	x:  0, // position
+	y:  0,
+	vx: 0, // velocity
+	vy: 0,
+	ax: 0, // acceleration
+	ay: 0,
+	k:  1, // dampening
   
 	init: function(options) {
 		options       = options 	  || {};
@@ -14,13 +21,39 @@ var BasicBody = Koi.define({
 		this.canvas   = options.canvas  || defaultCanvas;
 		this.ctx      = options.ctx     || defaultContext;
 		this.name     = options.name    || 'Unnamed Drawable';
+		this.y = options.y   || 0;
+		this.ay = options.ay || 0;
+		this.vy = options.vy || 0;
+	},
+
+	setAccel: function(ax, ay){
+		this.ax = ax;
+		this.ay = ay;
+	},
+
+	setSpeed: function(vx, vy){
+		this.vx = vx;
+		this.vy = vy;
+	},
+
+	move: function(){
+		this.ax *= this.damp;
+		this.ay *= this.damp;
+		this.vx += this.ax;
+		this.vy += this.ay;
+		this.x  += this.vx;
+		this.y  += this.vy;
 	},
 
 	draw: function(){
+		this.move();
 		var c = this.ctx;
+		// canvas hates floats
+		var x = parseInt(this.x, 10);
+		var y = parseInt(this.y, 10);
 		c.save();
 		c.font = '12px';
-		c.fillText(this.name, 0, 12);
+		c.fillText(this.name, x, y + 12);
 		c.restore();
 		this.isDrawn = true;
 	}
---
+++ Paddle.js
@@ -0,0 +1,3 @@
+var Paddle = Koi.extend(BasicBody, {
+
+});
